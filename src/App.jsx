45d8a3a3c816f029fdca976743e826172c0762fc
marie-bcc <127097512+marie-bcc@users.noreply.github.com>
2023-09-01 13:37:53 +0200

    add function todo

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,16 @@ function App() {
     {id: nanoid(5),content: "item 3"}, 
   ])
 
+  const [newTodo, setNewTodo] = useState('')
+
   function deleteTodo(id) {
     setTodo(todo.filter(todo => todo.id !== id))
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTodo([...todo, {id: nanoid(5), content: newTodo}])
+    setNewTodo('')
   }
 
   return (
@@ -18,14 +26,24 @@ function App() {
       <div className="max-w-4xl mx-auto pt-20 px-6">
         <h1 className="text-3xl text-slate-100 mb-4">TODO LIST</h1>
 
-        <form className="mb-10">
+        <form 
+        onSubmit={handleSubmit}
+        className="mb-10">
           <label htmlFor="todo-item"
           className="text-slate-50">Nouvelle tâche</label>
-          <input type="text" className="mt-1 block w-full rounded"/>
+          <input 
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          type="text" 
+          className="mt-1 block w-full rounded"/>
           <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
         </form>
         <ul>
-          {todo.map(item => (
+          {todo.length === 0 && (
+            <li className='text-slate-50 text-md'> Aucunes tâches à afficher</li>
+          )}
+          {todo.length > 0 && 
+          todo.map(item => (
             <ListItem 
             key={item.id} 
             itemData={item} 
