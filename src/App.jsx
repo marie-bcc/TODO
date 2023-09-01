e2c4daa536259fe2c9e7160927e62c711b329f4b
marie-bcc <127097512+marie-bcc@users.noreply.github.com>
2023-09-01 14:28:12 +0200

    Add showValidation

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,27 @@ function App() {
   ])
 
   const [newTodo, setNewTodo] = useState('')
+  const [showValidation, setShowValidation] = useState(false)
 
   function deleteTodo(id) {
     setTodo(todo.filter(todo => todo.id !== id))
   }
 
-  function handleSubmit(e) {
+  function handleSubmit(e){
     e.preventDefault()
+    if (newTodo === ""){
+      setShowValidation(true)
+      return
+    }
+      
     setTodo([...todo, {id: nanoid(5), content: newTodo}])
+
     setNewTodo('')
+    setShowValidation(false)
   }
 
   return (
-    <div className="h-screen bg-slate-900">
+    <div className="h-screen">
       <div className="max-w-4xl mx-auto pt-20 px-6">
         <h1 className="text-3xl text-slate-100 mb-4">TODO LIST</h1>
 
@@ -36,6 +44,9 @@ function App() {
           onChange={(e) => setNewTodo(e.target.value)}
           type="text" 
           className="mt-1 block w-full rounded"/>
+          {showValidation && (
+            <p className='text-red-400'>Veuillez entrer une tâche</p>
+          )}
           <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
         </form>
         <ul>
